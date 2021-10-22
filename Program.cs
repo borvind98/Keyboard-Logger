f084cfc006619cbaf4ff8ac493ecbee00ea0f211
borvind98 <borvind98@gmail.com>
2021-10-22 11:25:58 +0200

    initialized project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Keyboard_Logger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
